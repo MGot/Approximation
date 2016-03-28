70f3c85f7d7afd94284fd9a481ba73245e272016
MGot <marcin.gotszling@gmail.com>
2016-03-28 10:12:44 +0200

    Almost finall

--- 1.py
+++ 1.py
@@ -12,16 +12,23 @@ for i in range(0,n):
 	for j in range(i,n):
 		if i != j:
 			G.add_edge(i,j,weight = random.randint(0,100))
-nx.draw(G)
 
 T = nx.minimum_spanning_tree(G)
 M = naa.min_maximal_matching(T)
+
+for i in M:
+	T.add_edge(*i)
+
+E = nx.eulerian_circuit(T)
+
 print ()
 print (G.edges(data=True))
 print ()
 print (T.edges(data=True))
 print ()
 print (M)
+print ()
+print (E)
 
-
+nx.draw(T, pos=None, arrows=True, with_labels=True)
 plt.show()
