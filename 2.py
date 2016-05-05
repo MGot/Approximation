a67fb8a50b9a45aa2e4e270e5a9c2d3930425d26
jopk <biasejok@gmail.com>
2016-05-05 14:22:23 +0200

    renamed import, transpose, printing LP

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 import sys
 import math
-import string as str
+import string
 
 def parseLeftSide(cStr, varCount, OptFunc):
 	cStr = cStr.replace(" ", "").replace("*", "")
@@ -16,7 +16,7 @@ def parseLeftSide(cStr, varCount, OptFunc):
 
 	tmpStr = cStr[0]
 	for i in cStr[1:]+' ':
-		if i in str.digits:
+		if i in string.digits:
 			tmpStr = tmpStr + i
 		elif i in 'x+- ':
 			if len(tmpStr) == 1:
@@ -70,6 +70,35 @@ def readFile(path):
 		print("Blad przy czytaniu pliku")
 	return C, A, b, s
 
+
+def transpose(A):
+    return [[A[j][i] for j in range(0, len(A))] for i in range(0, len(A[0]))]
+
+def print_lp(A, b, c, s):
+    max = find_max_long_of_int(A, b, c)
+    xes = len(str(len(A[0])))
+    str1 = "{{p:3}}{{a:>{}}}{{g:1}}{{x:1}}{{j:<{}}}".format(max, xes)           # '{plus:3}{Aij:>[max]}{gwiazdka:1}{x:1}{indeks_x:<[xes]}'
+    str2 = "  {{z:<2}}  {{b:>{}}}".format(max)                                  # '  {znak:<2}  {bi:>5}'
+    n = len(A[0])
+    for r in range(0, len(A)):
+        row = A[r]
+        written = False
+        for c in range(0, len(row)):
+            if row[c] != 0:
+                print(str1.format(a=row[c], x="x", g="*", j=c+1, p=" + " if written else ""), end="")
+                written = True
+            else:
+                print(str1.format(a="", x="", g="", j="", p=""), end="")
+        print(str2.format(z=s[r], b=b[r]))
+
+def find_max_long_of_int(A, b, c):
+    x = [
+        min([y if y < 0 else -y for y in b]),
+        min([y if y < 0 else -y for y in c]),
+        min([y if y < 0 else -y for r in A for y in r])
+    ]
+    return len(str(min(x)))
+
 C = []
 A = []
 b = []
@@ -81,3 +110,4 @@ print("C",C)
 print("A",A)
 print("b",b)
 print("s",s)
+print_lp(A, b, C, s)
